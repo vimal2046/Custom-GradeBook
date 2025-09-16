58851de854e93a44c918f212bd0d963f6f4792ab
vimal2046 <vimalchandar2003@gmail.com>
2025-09-16 13:21:03 +0530

    Increased the version to 2025091110

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025091109;   // YYYYMMDDXX format.
+$plugin->version   = 2025091110;   // YYYYMMDDXX format.
 $plugin->requires  = 2022041900;   // Moodle 4.0+.
 $plugin->component = 'gradeexport_customexcel';
 
